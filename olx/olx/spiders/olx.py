a19d8b549f424976464f37ed2e24289a86fdb6e9
taleshrocha <taleshrochaz@gmail.com>
2021-09-24 18:32:50 -0300

    Goes to each house page and extracts the info.

--- olx/olx/spiders/olx.py
+++ olx/olx/spiders/olx.py
@@ -2,26 +2,21 @@ import scrapy
 
 class olx(scrapy.Spider):
     name = 'olx'
-    #start_urls = ['https://rn.olx.com.br/imoveis']
-    start_urls = ['https://rn.olx.com.br/imoveis/venda']
-    #handle_httpstatus_list = [403]
+    start_urls = ['https://rn.olx.com.br/imoveis/venda?sf=1']
 
     def parse(self, response):
-        for houses in response.css('div.fnmrjs-1.gIEtsI'):
-            try:
-                yield {
-                        'name' : houses.css('h2.sc-1iuc9a2-1.dTvKuJ.sc-ifAKCX.eKQLlb::text').get(),
-                        'price' : houses.css('p.sc-1iuc9a2-8.bTklot.sc-ifAKCX.eoKYee::text').get().replace('R$ ', ''),
-                        'link' : response.css('a.fnmrjs-0.fyjObc').attrib['href'],
-                        }
-            except:
-                yield {
-                        'name' : houses.css('h2.sc-1iuc9a2-1.dTvKuJ.sc-ifAKCX.eKQLlb::text').get(),
-                        'price' : 'Sold Out',
-                        'link' : response.css('a.fnmrjs-0.fyjObc').attrib['href'],
-                        }
+        houseLinks = response.css('li.sc-1fcmfeb-2.juiJqh a')
+        yield from response.follow_all(houseLinks, self.parse_house)
 
-        nextPage = response.css('a.sc-248j9g-1.lfGTeV').attrib['href']
-        if nextPage is not  None:
-            yield response.follow(nextPage, callback=self.parse)
+        nextPageLinks = response.css('div.sc-hmzhuo.ccWJBO.sc-jTzLTM.iwtnNi a')
+        yield from response.follow_all(nextPageLinks, self.parse)
 
+    def parse_house(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='EMPTY').strip()
+
+        yield{
+            #'title' : extract_with_css('h2.sc-1iuc9a2-1.dTvKuJ.sc-ifAKCX.eKQLlb::text'),
+            'price' : extract_with_css('h2.sc-1wimjbb-0.JzEH.sc-ifAKCX.cmFKIN::text').replace('R$ ', ''),
+            #'link' : extract_with_css('a.fnmrjs-0.fyjObc::attr(href)'),
+        }
