ac28a68508283de6f39699036199245d052889f6
taleshrocha <taleshrochaz@gmail.com>
2021-10-01 17:12:03 -0300

    Some bug fixeS

--- olx/olx/spiders/Olx.py
+++ olx/olx/spiders/Olx.py
@@ -1,19 +1,21 @@
 import scrapy
 import re
 
+
 class Olx(scrapy.Spider):
     beginInter = 0
     endInter = 100000
-
     name = 'olx'
     start_urls = ['https://rn.olx.com.br/imoveis/venda?pe=' + str(endInter) + '&ps=' + str(beginInter)]
 
     def parse(self, response):
 
+        beginInter = 0
+        endInter = 100000
         string = response.css('span.sc-1mi5vq6-0.eDXljX.sc-ifAKCX.fhJlIo::text').get()
         match = re.search(pattern = "[0-9]*\.[0-9]*", string = string)
 
-        if float(match.group()) < 4800:
+        if float(match.group()) < 4.800:
             # Gets the links for each house in the page
             houseLinks = response.css('li.sc-1fcmfeb-2.juiJqh a')
             yield from response.follow_all(houseLinks, self.parse_house)
@@ -25,8 +27,7 @@ class Olx(scrapy.Spider):
             nextInterval = 'https://rn.olx.com.br/imoveis/venda?pe=' + str(endInter) + '&ps=' + str(beginInter)
             yield from response.follow_all(nextInterval, self.parse)
         else:
-            yield 'Nothing to scrape'
-            exit()
+         raise CloseSpider('Nothing to scrape')
 
     def parse_house(self, response):
 
@@ -49,9 +50,9 @@ class Olx(scrapy.Spider):
         yield{
             #'titulo' : extract_with_css('h1.sc-45jt43-0.eCghYu.sc-ifAKCX.cmFKIN::text'),
             'preco' : extract_with_css('h2.sc-1wimjbb-0.JzEH.sc-ifAKCX.cmFKIN::text').replace('R$ ', ''),
-            'area' : area,
-            'municipio' : municipio,
-            'categoria' : categoria,
-            'link' : response,
+            #'area' : area,
+            #'municipio' : municipio,
+            #'categoria' : categoria,
+            #'link' : response,
             #'value' : value,
         }
