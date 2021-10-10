dc155a40dd3dcb539fde7fec5ea20f887d38c7eb
taleshrocha <taleshrochaz@gmail.com>
2021-10-09 21:28:34 -0300

    New intervals links working.

--- olx/olx/spiders/Olx.py
+++ olx/olx/spiders/Olx.py
@@ -6,46 +6,52 @@ logger = logging.getLogger('LOGGER')
 
 class Olx(scrapy.Spider):
     def __init__(self):
-            self.ps = 0
+            self.ps = 90000
             self.pe = 100000
 
-    def next_link(pe):
-            self.ps = self.pe
-            self.pe = self.pe + 50000
+    def next_link(self, pe):
+            self.ps = pe
+            self.pe = pe + 50000
 
     name = 'olx'
-    start_urls = ['https://rn.olx.com.br/imoveis/venda?pe=1000&ps=10']
+    start_urls = ['https://rn.olx.com.br/imoveis/venda?pe=100000&ps=90000']
 
+    # Initial parser for Scrapy. Is the "main" function.
     def parse(self, response):
-        logger.info('====================START PARSER====================')
+        logger.info('====================START-PARSER====================')
+        logger.info('====================START-INTERVAL: %d-%d====================', self.ps, self.pe)
 
+        # Gets how many houses are in the page. The maximun is 5000.
         string = response.css('span.sc-1mi5vq6-0.eDXljX.sc-ifAKCX.fhJlIo::text').get()
-        logger.info('==================== %s ====================', string)
-        #match = re.search(pattern = "[0-9]*\.[0-9]*", string = string)
-        match = re.search(pattern = "[0-9][0-9]", string = string)
+        houseQuantity = re.search(pattern = "[0-9][0-9]", string = string)
+        logger.info('====================houseQuatity: %s====================', houseQuantity.group())
 
-        if float(match.group()) < 4800:
-            # Gets the links for each house in the page
-            houseLinks = response.css('li.sc-1fcmfeb-2.juiJqh a')
-            yield from response.follow_all(houseLinks, self.parse_house)
+        #if float(houseQuantity.group()) < 4800000:
+        # Gets the links for each house in the page
+        houseLinks = response.css('li.sc-1fcmfeb-2.juiJqh a')
+        yield from response.follow_all(houseLinks, self.parse_house)
 
-            page = response.css('div.sc-hmzhuo.ccWJBO.sc-jTzLTM.iwtnNi span::text').get()
-            if page == 'Página anterior':
+        # Gets the next page link.
+        nextPage = response.css('div.sc-hmzhuo.ccWJBO.sc-jTzLTM.iwtnNi span::text').get()
+        logger.info('====================nextPage: %s====================', nextPage)
+        if nextPage == 'Página anterior': # There is no more pages to scrape in this price interval.
             # Go to the next interval
-                nextLink = 'https://rn.olx.com.br/imoveis/venda?pe=' + str(self.pe) + '&ps=' + str(self.ps)
-                logger.info('====================Next link: %s====================', nextLink)
-                yield response.follow_all(nextLink, self.parse)
-            else:
-                # Get to a smaller interval TODO
-                logger.info('====================NEXT-PAGE====================')
-                nextPageLinks = response.css('div.sc-hmzhuo.ccWJBO.sc-jTzLTM.iwtnNi a')
-                yield from response.follow_all(nextPageLinks, self.parse)
+            self.next_link(self.pe)
+            logger.info('====================NEW-INTERVAL: %d-%d====================', self.ps, self.pe)
+            nextLink = 'https://rn.olx.com.br/imoveis/venda?pe=' + str(self.pe) + '&ps=' + str(self.ps)
+            logger.info('====================NEXT-LINK: %s====================', nextLink)
+            yield scrapy.Request(url=nextLink, callback=self.parse)
         else:
-            logger.info('====================DIED====================')
-            raise CloseSpider('Nothing to scrape')
+            logger.info('====================NEXT-PAGE====================')
+            nextPageLinks = response.css('div.sc-hmzhuo.ccWJBO.sc-jTzLTM.iwtnNi a')
+            yield from response.follow_all(nextPageLinks, self.parse)
+        #else:
+        #    logger.info('====================DIED====================')
+        #    raise CloseSpider('Nothing to scrape')
 
+    # Extracts all the needed information of a house page.
     def parse_house(self, response):
-        logger.info('====================PARCE-HOUSE====================')
+        #logger.info('====================PARCE-HOUSE====================')
 
         def extract_with_css(query):
             return response.css(query).get(default='EMPTY').strip()
