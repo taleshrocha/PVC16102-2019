82da71fc597075afcbfd05e4c9ebadcb8e6a4f86
taleshrocha <taleshrochaz@gmail.com>
2021-10-14 17:47:56 -0300

    Better regex and now it gets the CEP

--- olx/olx/spiders/Olx.py
+++ olx/olx/spiders/Olx.py
@@ -65,10 +65,12 @@ class Olx(scrapy.Spider):
         areaUtil = 'AREAUTIL-ERR'
         areaTitle = 'AREATITLE-ERR'
         areaDesc = 'AREADESC-ERR'
+        areaDesc = 'AREADESC-ERR'
         areaUtil = 'AREAUTIL-ERR'
         areaConst = 'AREACONST-ERR'
         municipio = 'MUNICIPIO-ERR'
         categoria = 'CATEGORIA-ERR'
+        cep = 'CEP-ERR'
 
         houseDetails = response.css('div.duvuxf-0.h3us20-0.jyICCp')
 
@@ -79,12 +81,14 @@ class Olx(scrapy.Spider):
                 areaConst = detail.css('dd::text').get().replace('m²', '')
             elif detail.css('dt::text').get() == 'Município':
                 municipio = detail.css('dd::text').get()
+            elif detail.css('dt::text').get() == 'CEP':
+                cep = detail.css('dd::text').get()
             elif detail.css('dt::text').get() == 'Categoria':
                 categoria = detail.css('a::text').get()[:-1]
 
         # Gets the area by the house title. If it has
         title = response.css('h1.sc-45jt43-0.eCghYu.sc-ifAKCX.cmFKIN::text').get()
-        aux = re.search(pattern = "\d+\.?\d+?\s?m²?2?", string = title) # Gets strings like '60m²' or '60 m²' or '60.01m2' and more
+        aux = re.search(pattern = '\d+(\.|,)?(\d+)?(\s+)?m(²|2)', string = title) # Gets strings like '60m²' or '60 m²' or '60.01m2' and more
         if aux != None:
             areaClear = re.search(pattern = "\d+", string = aux.group()) # Gets only the numerator value
             areaTitle = areaClear.group()
@@ -97,13 +101,23 @@ class Olx(scrapy.Spider):
         elif areaTitle != 'AREATITLE-ERR':
             area = areaTitle
 
+        description = response.css('span.sc-1sj3nln-1.eOSweo.sc-ifAKCX.cmFKIN::text').get()
+        aux = re.search(pattern = '\d+(\.|,)?(\d+)?(\s+)?m(²|2)', string = description) # Gets strings like '60m²' or '60 m²' or '60.01m2' and more
+        if aux != None:
+            areaClear = re.search(pattern = "\d+", string = aux.group()) # Gets only the numerator value TODO This will make a 2.000m2 how have 2m2!
+            # TODO If the float point is ',' change to '.'
+            areaDesc = areaClear.group()
+
         yield{
-            'titulo' : extract_with_css('h1.sc-45jt43-0.eCghYu.sc-ifAKCX.cmFKIN::text', 'TITULO-ERR'),
-            #'preco' : extract_with_css('h2.sc-1wimjbb-0.JzEH.sc-ifAKCX.cmFKIN::text', 'PRECO-ERR').replace('R$ ', ''),
+            #'titulo' : extract_with_css('h1.sc-45jt43-0.eCghYu.sc-ifAKCX.cmFKIN::text', 'TITULO-ERR'),
+            #'description' : description,
+            'preco' : extract_with_css('h2.sc-1wimjbb-0.JzEH.sc-ifAKCX.cmFKIN::text', 'PRECO-ERR').replace('R$ ', ''),
             'area' : area,
+            'cep' : cep,
             #'areaUtil' : areaUtil,
             #'areaConst' : areaConst,
-            'areaTitle' : areaTitle,
+            #'areaTitle' : areaTitle,
+            #'areaDesc' : areaDesc,
             #'municipio' : municipio,
             #'categoria' : categoria,
             #'link' : self.link_extractor.extract_links(response).get(),
